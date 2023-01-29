a8a78881a99f8b04d5fd8223f088b5a004a24bf7
Gustavo Garcia <dragulias1@gmail.com>
2023-01-28 21:59:09 -0500

    montado el scanner en el boton con lineamientos de flutter_barcode_scanner

--- lib/widgets/scan_botton.dart
+++ lib/widgets/scan_botton.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';//necesaria para utilizar el cam scanner.. esta en la documentacion 
 
+
+//ESTE ES EL BOTON DE SCANNER
 class ScanBotton extends StatelessWidget {
   const ScanBotton({super.key});
 
@@ -7,7 +10,11 @@ class ScanBotton extends StatelessWidget {
   Widget build(BuildContext context) {
     return FloatingActionButton(
       elevation: 0,//esta elevacion la ha quitado tambien de otro widget quede jumm
-    onPressed: (){},
+    onPressed: ()async{
+      String barcodeScanRes = await
+       FlutterBarcodeScanner.scanBarcode('#3D8BEF','CANCELAR',false,ScanMode.QR);
+       //pide: un color, mensaje de cancelar,, activar flash, y que es lo que va a hacer,, en este caso .QR
+    },
     child:Icon(Icons.filter_center_focus)
     );
   }
